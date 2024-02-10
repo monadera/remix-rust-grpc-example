3af62e1830cd1e03a5e4918fe4d478e0d5836501
David Steiner <david_j_steiner@yahoo.co.nz>
2024-02-10 15:11:06 +0100

    Configure tracing

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,4 +1,22 @@
+use anyhow::Result;
+use tracing::info;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
 #[tokio::main]
-async fn main() {
-    println!("This will be a trading application!");
+async fn main() -> Result<()> {
+    dotenvy::dotenv()?;
+    setup_tracing_registry();
+    info!("This will be a trading application!");
+
+    Ok(())
+}
+
+fn setup_tracing_registry() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "info,trading_api=debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer().pretty())
+        .init();
 }
